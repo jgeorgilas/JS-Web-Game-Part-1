e55ac2727b310af0f26d86053510f38939398f76
jgeorgilas <120068389+jgeorgilas@users.noreply.github.com>
2023-02-26 12:02:53 -0800

    Added images

--- index.js
+++ index.js
@@ -1,6 +1,23 @@
-let greenCharacter = document.createElement('img')
-greenCharacter.src = 'assets/green-character.gif'
-greenCharacter.style.position = 'fixed'
-greenCharacter.style.left = '100px'
-greenCharacter.style.bottom = '100px'
-document.body.append(greenCharacter)
+
+    const newImage = (source, position, left, bottom) => {
+        let imgObject = document.createElement('img')
+        imgObject.src = source
+        imgObject.style.position = position
+        imgObject.style.left = left
+        imgObject.style.bottom = bottom
+        document.body.append(imgObject)
+        &&
+        object.addEventListener('dblclick', () => {
+                object.remove()
+            })
+        }
+    
+newImage('assets/green-character.gif','fixed','100px','100px')    
+newImage('assets/tree.png','fixed','200px','300px')
+newImage('assets/pillar.png','fixed','350px','100px')
+newImage('assets/crate.png','fixed','150px','200px')
+newImage('assets/well.png','fixed','500px','425px')
+newImage('assets/shield.png','fixed','165px','185px')
+newImage('assets/staff.png','fixed','600px','100px')
+
+
